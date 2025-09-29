b963850d2122732c4a831f80aa556a823a366183
Luegiu Yuusha-sama <cassianofpereira@gmail.com>
2025-09-29 18:32:51 -0300

    Ok pfv funcione

--- js/perfil-cachorro.js
+++ js/perfil-cachorro.js
@@ -3,46 +3,48 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const id = params.get("id");
 
-    let response = await fetch(`https://api.webmercadoria.com.br/api/pet/?id=${id}`);
-    let data = await response.json();
-    let pet = data.pet[0];
+    let response = await fetch(`https://api.webmercadoria.com.br/api/pet/?id=${id}`)
+        .then(res => res.json())
+        .then(data => {
+            let pet = data.pet[0];
 
-    let caixaCards = document.getElementById("caixa-card-perfil-dog");
+            let caixaCards = document.getElementById("caixa-card-perfil-dog");
 
-    let card = document.createElement("div");
+            let card = document.createElement("div");
 
-    let img = document.createElement("img");
-    img.src = "../../cachorro-em-estudio.jpg";
-    img.alt = `Foto do ${pet.name}`;
-    card.className = "card";
+            let img = document.createElement("img");
+            img.src = "../../assets/cachorro-em-estudio.jpg";
+            img.alt = `Foto do ${pet.name}`;
+            card.className = "card";
 
-    let petName = document.createElement("h3");
-    petName.textContent = pet.name;
+            let petName = document.createElement("h3");
+            petName.textContent = pet.name;
 
-    let petType = document.createElement("span");
-    petType.textContent = pet.type;
+            let petType = document.createElement("span");
+            petType.textContent = pet.type;
 
-    let petVaccines = document.createElement("p");
-    petVaccines.textContent = `Vacinas: ${pet.vaccines}`;
+            let petVaccines = document.createElement("p");
+            petVaccines.textContent = `Vacinas: ${pet.vaccines}`;
 
-    let petAge = document.createElement("p");
-    petAge.textContent = `Idade: ${pet.age}`;
+            let petAge = document.createElement("p");
+            petAge.textContent = `Idade: ${pet.age}`;
 
-    let petSize = document.createElement("p");
-    petSize.textContent = pet.animalSize;
+            let petSize = document.createElement("p");
+            petSize.textContent = pet.animalSize;
 
-    let petLocale = document.createElement("p");
-    petLocale.textContent = pet.locale;
+            let petLocale = document.createElement("p");
+            petLocale.textContent = pet.locale;
 
-    let petMessage = document.createElement("span");
-    petMessage.textContent = "Cachorro dócil e brincalhão, com um pouco de atenção ele se apega a você";
+            let petMessage = document.createElement("span");
+            petMessage.textContent = "Cachorro dócil e brincalhão, com um pouco de atenção ele se apega a você";
 
-    card.appendChild(img);
-    card.appendChild(petName);
-    card.appendChild(petType);
-    card.appendChild(petVaccines);
-    card.appendChild(petAge);
-    card.appendChild(petSize);
-    card.appendChild(petLocale);
-    caixaCards.appendChild(card);
+            card.appendChild(img);
+            card.appendChild(petName);
+            card.appendChild(petType);
+            card.appendChild(petVaccines);
+            card.appendChild(petAge);
+            card.appendChild(petSize);
+            card.appendChild(petLocale);
+            caixaCards.appendChild(card);
+        });
 });
